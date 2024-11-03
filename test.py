8287c6f9e62be0076751a416a697178d31533605
EillesWan <W-YI_DoctorYI@outlook.com>
2024-11-03 17:09:59 +0800

    测测时区更改

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 from zhDateTime import DateTime, int_hànzìfy
 
 import random
+import datetime
+
+# 时区相差
+
+print(
+    DateTime.now(
+        datetime.timezone(
+            datetime.timedelta(hours=8, minutes=-40),
+        )
+    )
+    .to_lunar()
+    .hanzify()
+)
 
 # import zhdate
 
@@ -12,6 +25,7 @@ print(
         it := random.randint(1000000, 10000000000000000), int_hànzìfy(it)
     )
 )
+
 
 # print(DateTime.from_lunar(zhdate.ZhDate.today().lunar_year,zhdate.ZhDate.today().lunar_month,zhdate.ZhDate.today().leap_month,zhdate.ZhDate.today().lunar_day).to_lunar().hànzì())
 
